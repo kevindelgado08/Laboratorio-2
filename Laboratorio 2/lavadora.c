8eb2e121869f86df51042c05bd75fb853a1757b2
andresnv12 <andres.chavesvargas@ucr.ac.cr>
2023-04-19 17:43:37 -0600

    Cambios realizados en la escritura de las variables, se eliminaron interrupciones que estaban siendo llamadas más de una vez y se pusieron fuera de los cases debido a que dentro no se pueden utilizar.

--- Laboratorio 2/lavadora.c
+++ Laboratorio 2/lavadora.c
@@ -2,138 +2,109 @@
 #include <avr/interrupt.h>
 
 
-int t_suministro, t_lavar, t_enjuagar, t_centrigar; //tiempos de cada estado
-int Sb, Lb, Eb, Cb = 0b00000001, 0b00000011, 0b00000010, 0b00000011;
-int Sm, Lm, Em, Cm = 0b00000010, 0b00000111, 0b00000100, 0b00000110;
-int Sa, La, Ea, Ca = 0b00000011, 0b00001010, 0b00000101, 0b00001001;
-sei(); //habilita las interrupciones globales
-interrupciones();
+unsigned char t_suministro, t_lavar, t_enjuagar, t_centrigar; //tiempos de cada estado
+//unsigned char Estado, Asignar_Carga, Inicio, Suministro_Agua, Lavar, Enjuagar, Centrifugar, Pausa;
+unsigned char S_Inicio, S_suministro, S_Lavar, S_Enjuagar, S_Centrifugar;
+unsigned char Estado;
+
+unsigned char Sb = 0b00000001, Lb = 0b00000011, Eb = 0b00000010, Cb = 0b00000011;
+unsigned char Sm = 0b00000010, Lm = 0b00000111, Em = 0b00000100, Cm = 0b00000110;
+unsigned char Sa = 0b00000011, La = 0b00001010, Ea = 0b00000101, Ca = 0b00001001;
+void estados();
+void interrupciones();
 int main(void)
 {
-  GIMSK, PCMSK0 , PCMSK1 , PCMSK2  = 0b01111000, 0b00000001, 0b00000001, 0b00000001; //Habilitación de Pines
+  
+  DDRD = 0b00110000;
+ 
+  GIMSK = 0b01111000;
+  PCMSK = 0b00000001;
+  PCMSK1 = 0b00000001;
+  PCMSK2 = 0b00000001; //Habilitación de Pines
   while(1){
-     
-     estados();
+    interrupciones();
+    estados();
   }
 }
 
+void interrupciones()
+{
+  sei(); //habilita las interrupciones globales
+}
+ISR(PCINT0_vect) //
+      {
+        t_suministro = Sb;
+        t_lavar = Lb;
+        t_enjuagar = Eb; 
+        t_centrigar = Cb;
+        GIMSK &= ~(1<<PCIE1);//Se deshabilitan los pines para evitar un error
+        GIMSK &= ~(1<<PCIE2);
+        Estado = 'b';
+        PORTD = 0b00110000; 
+      }
 
-  
+ISR(PCINT1_vect) //
+    {
+      t_suministro = Sm;
+      t_lavar = Lm;
+      t_enjuagar = Em; 
+      t_centrigar = Cm;
+      GIMSK &= ~(1<<PCIE0); //Se deshabilitan los pines para evitar un error
+      GIMSK &= ~(1<<PCIE2);
+      Estado = 'b';
+    }
+ISR (PCINT2_vect) //
+    {
+      t_suministro = Sa;
+      t_lavar = La;
+      t_enjuagar = Ea; 
+      t_centrigar = Ca;
+      GIMSK &= ~(1<<PCIE0); //Se deshabilitan los pines para evitar un error
+      GIMSK &= ~(1<<PCIE1);
+      Estado = 'b';
+    }
 
-int estados() //procede a llamar los estados de la lavadora
+
+ISR (INT0_vect)
+  {
+    Estado = 'g';
+  }
+
+ISR (TIMER0_COMPA_vect)
+{
+  S_suministro = 0;
+  Estado = 'd';
+}
+ 
+
+void estados(void) //procede a llamar los estados de la lavadora
 {
 
 switch(Estado)
 {
-  Estado = Asignar_Carga;
-  case Asignar_Carga: //Estado inicial
+  Estado = 'a';
+  case 'a': //Estado inicial
+    
 
-    ISR_BAJA(PCIF0) //al habilitarse cambia el valor de Estado a Suministro de agua
-        {
-          t_suministro = Sb;
-          t_lavar = Lb;
-          t_enjuagar = Eb; 
-          t_centrigar = Cb;
-          GIMSK &= ~(1<<PCIE1);//Se deshabilitan los pines para evitar un error
-          GIMSK &= ~(1<<PCIE2);
-          Estado = Inicio;
-
-        }
-
-    ISR_MEDIA(PCIF1) //al habilitarse cambia el valor de Estado a Suministro de agua
-        {
-          t_suministro = Sm;
-          t_lavar = Lm;
-          t_enjuagar = Em; 
-          t_centrigar = Cm;
-          GIMSK &= ~(1<<PCIE0); //Se deshabilitan los pines para evitar un error
-          GIMSK &= ~(1<<PCIE2);
-          Estado = Inicio;
-        }
-    ISR_ALTA(PCIF2) //al habilitarse cambia el valor de Estado a Suministro de agua
-        {
-          t_suministro = Sa;
-          t_lavar = La;
-          t_enjuagar = Ea; 
-          t_centrigar = Ca;
-          GIMSK &= ~(1<<PCIE0); //Se deshabilitan los pines para evitar un error
-          GIMSK &= ~(1<<PCIE1);
-          Estado = Inicio;
-        }
-
-  case Inicio:
+  case 'b':
     S_Inicio = 1;
     
-      ISR_SUMINISTRO(INTF0) //al habilitarse cambia el valor de Estado a Suministro de agua
-      {
-        S_Inicio = 0;
-        Estado = Suministro_Agua;
 
-      }
-
-    //break;
-
-  case Suministro_Agua:
-      S_suministro = 1;
+  case 'c':
+      S_suministro = 1;  
       
-      
-      ISR_PAUSA(PCIF0)
-      {
-        Estado = Pausa;
-      }
-
-      ISR_LAVAR(OCF0A)
-      {
-        S_suministro = 0;
-        Estado = Lavar;
-      }
-
-  case Lavar:
+  case 'd':
     S_Lavar = 1;
-    
-    ISR_PAUSA(PCIF0)
-    {
-      Estado = Pausa;
-    }
-    ISR_ENJUAGAR(OCF0A)
-    {
-      S_Lavar = 0;
-      Estado = Enjuagar;
-    }
-    
-  case Enjuagar:
+      
+  case 'e':
+    TCNT0 = t_enjuagar;
     S_Enjuagar = 1;
-    
-    ISR_PAUSA(PCIF0)
-    {
-      Estado = Pausa;
-    }
-
-    ISR_CENTRIFUGAR(OCF0A)
-    {
-      S_Enjuagar = 0;
-      Estado = Centrifugar;
-    }
-    
-  case Centrifugar:
+     
+  case 'f':
     S_Centrifugar = 1;
     
-    ISR_PAUSA(PCIF0)
-    {
-      Estado = Pausa;
-    }
-  
-    ISR_TERMINAR(OCF0A)
-    {
-      S_Centrifugar = 0;
-      Estado = Asignar_Carga;
-    }
+  case 'g':
+     break;
+}
+}
 
-  case Pausa:
-  
-    ISR_PAUSA(PCIF0)
-    {
-      Estado = Pausa;
-    }
-}
-}
