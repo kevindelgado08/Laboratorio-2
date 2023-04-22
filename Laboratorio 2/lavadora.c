c50c3e8df52b5696b0a260c1daed650ed54aac11
andresnv12 <andres.chavesvargas@ucr.ac.cr>
2023-04-22 08:12:07 -0600

    Cambios realizados:
    lavadora.simu:
    
    Se realizaron cambios en las conexiones a los pines y se cambiaron los switches por pulsadores.
    
    lavadora.c:
    
    Se añadieron los números que deben mostrarse en el display, así como cambios realizados en la función num_display() y se añadió la fórmula que permite cambiar el tiempo que dura la interrupción por comparación.

--- Laboratorio 2/lavadora.c
+++ Laboratorio 2/lavadora.c
@@ -1,15 +1,36 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
-#include <util/delay.h>
+
 //Variables Importantes
 unsigned char t_suministro, t_lavar, t_enjuagar, t_centrigar, Tiempo_Actual; //tiempos de cada estado
 unsigned char S_Inicio, S_suministro, S_Lavar, S_Enjuagar, S_Centrifugar; //Indicadores de Estado
-unsigned char Estado, Accion, Volver_Estado, Prox_Estado, prox_TCNT0, unid, dec, i, tiempo_estado;
+unsigned char Estado, Accion, Volver_Estado, Prox_Estado, Estado_Actual;
 unsigned char Sb = 0b00000001, Lb = 0b00000011, Eb = 0b00000010, Cb = 0b00000011; //Tiempo en Carga Baja
 unsigned char Sm = 0b00000010, Lm = 0b00000111, Em = 0b00000100, Cm = 0b00000110; //Tiempo en Carga Media
 unsigned char Sa = 0b00000011, La = 0b00001010, Ea = 0b00000101, Ca = 0b00001001; //Tiempo en Carga Alta
-int timer;
- 
+
+//Números a mostrar en el display
+unsigned const char numuni[]= {0b00000110, 
+0b00010110, 
+0b00100110, 
+0b00110110, 
+0b01000110, 
+0b01010110, 
+0b01100110, 
+0b01110110, 
+0b10000110, 
+0b10010110};
+unsigned const char numdeci[] = {0b00000110, 
+0b00010110, 
+0b00100110, 
+0b00110110, 
+0b01000110, 
+0b01010110, 
+0b01100110, 
+0b01110110, 
+0b10000110, 
+0b10010110};
+int timer, tiempo_estado;
 //Llamado a Funciones
 void estados();
 
@@ -19,9 +40,9 @@ int main(void)
   
   //Habilitaci\'on de GPIO'S
   DDRD = 0b01111000;
-  PORTD = 0b01000000;  
-  DDRB = 0b11110010;
-  PORTB = 0b00000010;
+  PORTD = 0b00000000;  
+  DDRB = 0b11110110;
+  PORTB = 0b00000110;
   DDRA = 0b00000010;
   PORTA = 0b00000000;
 
@@ -33,12 +54,11 @@ int main(void)
   PCMSK2 = 0b00000001;
 
   
-  // Configurar Timer Counter 0 en modo CTC con prescaler de 1024
+  // Configuración Timer Counter 1 en modo CTC con prescaler de 1024
 
-  
-  TCCR1A |= (1 << WGM13) | (1 << WGM12) | (0 << WGM10) | (0 << WGM11); // modo CTC
-  TCCR1B |= (1 << CS12) | (1 << CS10) | (0 << CS11); // prescaler de 1024
-  ICR1 = 30000; // valor de comparación
+  TCCR1A |= (1 << WGM13) | (1 << WGM12) | (0 << WGM10) | (0 << WGM11); 
+  TCCR1B |= (1 << CS12) | (1 << CS10) | (0 << CS11); 
+  ICR1 = (Tiempo_Actual * 1000000-1024)/2*1024; // valor de comparación deberia cambiar conforme cambia tiempo actual
   
   sei(); //habilita las interrupciones globales
     
@@ -46,6 +66,7 @@ int main(void)
   Estado = 'b';
   Accion = 0;
   Volver_Estado = 'a';
+  Tiempo_Actual = 0;
   while(1){
     
     estados();
@@ -53,110 +74,97 @@ int main(void)
 }
   
 //Función para mostrar en el display
-void num_display(tiempo_estado)
+int num_display(tiempo_estado)
 {
-   
+    int unid;
+    int dec;
+    int i;
     
 
     //Para multiplexar el numero en decena y unidad
     for (i = tiempo_estado; i<=0; i--)
     {
-      dec =i/10;
-      unid =i%10;
-      unid = unid << 4;
-      dec = dec << 4;
-
+      //Para multiplexar el numero en decena y unidad
+      dec=tiempo_estado/10;
+      unid=tiempo_estado%10;
+      //PORTB &= ~(1<<2);
       PORTB &= ~(0<<1);
-      PORTB = PORTB | (unid); 
-      TIMSK = (1 << OCIE1A);  
-      PORTB &= ~(0<<1);
-      TIMSK = (0 << OCIE1A);
-      PORTD &= ~(0<<6);           
-      PORTB = PORTB | (dec);
-      TIMSK = (1 << OCIE1A);
-      PORTD &= ~(0<<6);
-      TIMSK = (0 << OCIE1A);
-
+      PORTB |= numuni[unid];   
+      TIMSK |= (1 << TOIE1);           
+      PORTB |= numdeci[dec];
+      PORTB &= ~(0<<2);
+      //PORTB &= ~(1<<1);
     }
-}/*
-ISR (TIMER1_COMPA_vect)
-{
-  if(timer == 0)
-  {
-    Estado = Prox_Estado;
-  }
-  timer = Tiempo_Actual;
-  
-  Tiempo_Actual--;
-  timer--;
-
-  
-}
-*/
-//Version Original
-ISR (TIMER1_COMPA_vect)
-{
-
-  num_display(Tiempo_Actual);
-  
 }
 
- 
+
 void estados(void) //procede a llamar los estados de la lavadora
 {
-
+num_display();
 switch(Estado)
 { 
   case 'b': //Inicio
-    Accion = 0;
+    TIMSK = (0 << OCIE1A);
+    Accion = 0; //Permite que se de inicio al programa al pulsar el botón de inicio.
     PORTD &= ~(1<<5);
     PORTD &= ~(1<<4);
     PORTD &= ~(1<<3);
     PORTA &= ~(1<<1);
+    Tiempo_Actual = t_suministro;
     break; 
 
     
   case 'c': //Suministro de Agua
-    Tiempo_Actual = t_suministro; 
+    Tiempo_Actual = t_suministro;
+    ICR1 = (Tiempo_Actual * 1000000-1024)/2*1024;
+    TIMSK = (1 << OCIE1A); //habilita la interrupción
+    Prox_Estado = 'd';
+    S_suministro = 0, S_Lavar = 1, S_Enjuagar = 0, S_Centrifugar = 0, Accion = 1; // Estados que permiten volver de pausa
+    PORTD = PORTD | (1<<5); //enciende el led correspondiente
     
-    S_suministro = 0, S_Lavar = 1, S_Enjuagar = 0, S_Centrifugar = 0, Accion = 1;
-    PORTD = PORTD | (1<<5);
     num_display(Tiempo_Actual);
-    Estado = 'd';
-    ;  
-    
-    
-      break;
+    break;
 
   case 'd': //Lavar
-    Tiempo_Actual = t_lavar;  
+    
+    Tiempo_Actual = t_lavar; 
+    ICR1 = (Tiempo_Actual * 1000000-1024)/2*1024;
     Prox_Estado = 'e';
     S_suministro = 0, S_Lavar = 1, S_Enjuagar = 0, S_Centrifugar = 0, Accion = 1;
     PORTD = PORTD | (1<<4);
     PORTD &= ~(1<<5);
-    
-
+    num_display(Tiempo_Actual);
     break;
 
   case 'e': //Enjuagar
-    Tiempo_Actual = t_enjuagar;  
+    Tiempo_Actual = t_enjuagar; 
+    ICR1 = (Tiempo_Actual * 1000000-1024)/2*1024;
     Prox_Estado = 'f';
     S_suministro = 0, S_Lavar = 0, S_Enjuagar = 1, S_Centrifugar = 0, Accion = 1;
     PORTD = PORTD | (1<<3);
     PORTD &= ~(1<<4);
-    
+    num_display(Tiempo_Actual);
     break;
 
   case 'f': //Centrifugar
-    Tiempo_Actual = t_enjuagar;  
+      
     Prox_Estado = 'b';
     S_suministro = 0, S_Lavar = 0, S_Enjuagar = 0, S_Centrifugar = 1, Accion = 1;
-    PORTA = PORTA | (1<<1);
+    Tiempo_Actual = t_centrigar;
+    ICR1 = (Tiempo_Actual * 1000000-1024)/2*1024;
     PORTD &= ~(1<<3);
+    PORTA = PORTA | (1<<1);
+    num_display(Tiempo_Actual);
     break;
 
-  case 'g': //Pausa
+  case 'g': //Pausa 
+  //Al pulsar pausa de nuevo debería volver al estado correspondiente.
     
+    
+    PORTD &= ~(1<<5);
+    PORTD &= ~(1<<4);
+    PORTD &= ~(1<<3);
+    PORTA &= ~(1<<1);
     switch(Volver_Estado)
     {
       case 'a': //Volver a Suminitro de Agua
@@ -166,7 +174,8 @@ switch(Estado)
             Volver_Estado = 'b'; // pasa a revisar si se encuentra en estado Lavar
             break;
           case 1:
-            Estado = 'c'; //Vuelve a Suministro de Agua 
+            Accion = 2; //Vuelve a Suministro de Agua 
+            Estado_Actual = 'c';
             break;
         }
       case 'b': // Volver a Lavar
@@ -176,7 +185,8 @@ switch(Estado)
             Volver_Estado = 'c';
             break;
           case 1:
-            Estado = 'd';
+            Accion = 2;
+            Estado_Actual = 'd';
             break;
         }
       case 'c': // Volver a Enjuagar
@@ -184,9 +194,11 @@ switch(Estado)
         {
           case 0:
             Volver_Estado = 'd';
+            
             break;
           case 1:
-            Estado = 'e'; 
+            Accion = 2;
+            Estado_Actual = 'e';
             //asignar el valor de TCN0 de tal forma que al volver de la pausa, siga la cuenta en el mismo numero que se tiene
             break;
         }
@@ -194,7 +206,8 @@ switch(Estado)
         switch(S_Centrifugar)
         {
           case 1:
-            Estado = 'f';
+            Accion = 2;
+            Estado_Actual = 'f';
             break;
         }
     } 
@@ -255,7 +268,14 @@ ISR (INT0_vect)
       
       case 1: //Pausa
       Estado = 'g';
+      TIMSK = (0 << OCIE1A);
       break;
+      
+      case 2:
+      Estado = Estado_Actual;
+      Accion = 1;
+      TIMSK = (1 << OCIE1A);
+      
       
   
     }
@@ -264,6 +284,18 @@ ISR (INT0_vect)
 
 //Interrupción de Timeout de los estados:
 
+ISR (TIMER1_COMPA_vect)
+{
+  
+  Estado = Prox_Estado;
+  num_display(Tiempo_Actual);
+  
+}
+
+/*ISR (TIMER1_OVF_vect) //Conmutar displays
+{
+   
+}*/
 
 
 
